89a50023ec2bba798ee42f09112a4b84f271abfc
deni-liandi <34767027+deni-liandi@users.noreply.github.com>
2018-04-15 10:26:25 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,2 @@
 document.write('<script src="https://cdn.rawgit.com/deni-liandi/project/1e166740/js/jquery-3.3.1.js"></script>');
-alert('Test');
+document.write('<script src="https://cdn.rawgit.com/deni-liandi/project/3558d59d/js/bootstrap/bootstrap.js"></script>');
